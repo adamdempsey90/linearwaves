ec6be8d1db156089bc39c0e6eebc8bfa94418939
Adam Dempsey <adamdemps@gmail.com>
2016-11-14 14:23:29 -0600

    Working on fftw.

--- src/planet.c
+++ src/planet.c
@@ -1,10 +1,12 @@
 #include "linearwaves.h"
 #include <gsl/gsl_integration.h>
+#include <fftw3.h>
 typedef struct GSL_params {
     double x;
     int m;
 } GSL_params;
 
+fftw_plan pr2r,pr2r1d;
 
 void init_planet(void) {
 
@@ -19,7 +21,6 @@ void init_planet(void) {
 
 double potential(double phi, double x) {
     double res = -pow(x*x + planet.a*planet.a + planet.eps2 - 2*planet.a*x*cos(phi),-.5);
-
     if (planet.indirect) {
         res += cos(phi)*planet.a/(x*x);
     }
@@ -81,31 +82,44 @@ void force(double x, int m, double complex *res) {
     dr_res_r *= planet.mp/M_PI;
 
     res[0] = -dr_res_r ;
-    res[1] = res_r * -I * m/x;
+    res[1] = res_r ;//* -I * m/x;
     res[2] = 0;
     gsl_integration_workspace_free(w); 
     return;
 }
 
-void fft_init(void) {
-    
+void fft_init(double *in, double *out,int nphi) {
+    int rank = 1; 
+    int n[] = {nphi}; /* 1d transforms of length 10 */
+    int howmany = params.n;
+    int idist = nphi;
+    int odist = nphi;
+    int istride = 1;
+    int ostride = 1; /* distance between two elements in 
+                                                               the same column */
+    int *inembed = n, *onembed = n;
+    fftw_r2r_kind kind[] = {FFTW_REDFT00};
 
+    pr2r = fftw_plan_many_r2r(rank, n, howmany,in,inembed, istride, idist, out, onembed,ostride,odist, kind, FFTW_ESTIMATE);
 
-
+    pr2r1d = fftw_plan_r2r_1d(nphi, in, out,FFTW_REDFT00, FFTW_ESTIMATE);
 }
 
-void do_fft(double *in, double complex *out) {
-    
-    fftw_excecute(pr2c);
-
-
-
+void do_fft(double *in, double *out) {
+    fftw_execute(pr2r1d);
+    return;
+}
+void free_fft(double *in, double *out) {
+    free(in);
+    free(out);
+    fftw_destroy_plan(pr2r);
+    fftw_destroy_plan(pr2r1d);
 }
 
 
 int main(int argc, char *argv[]) {
 
-    planet.mp = 1e-5;
+    planet.mp = 1;
     planet.a = 1.0;
     planet.eps = .6*.05;
     planet.eps2 = planet.eps*planet.eps;
@@ -115,29 +129,37 @@ int main(int argc, char *argv[]) {
         printf("Using indirect potential\n");
     }
 
-   
-    int NM = atoi(argv[1]);
-    int NR = atoi(argv[2]);
-
-
-    double complex *res = (double complex *)malloc(sizeof(double complex)*NM*NR*3);
+    int NM = 100;
+    int NR = 10;
+    double rmin = .1;
+    double rmax = 1.5;
+    params.n = NR;
     
+    double complex res[3];
     int m;
-    double x;
+    double x = .8;
     int j,indx;
-    for(m=0;m<NM;m++) {
-        for(j=0;j<NR;j++) {
-            x = .6 + j*(2.-.6)/NR;
-            indx = j*3 + m*NR*3;
-            force(x,m,&res[indx]);
-        }
+    int nphi = 200;
+    double phi;
+    double *out = (double *)malloc(sizeof(double)*nphi);
+    double *in = (double *)malloc(sizeof(double)*nphi);
+    force(x,2,res);
+    printf("%lg\n",creal(res[1]));
+    for(j=0;j<nphi;j++) {
+        phi = 2*M_PI/(nphi-1) *j;
+        in[j] = potential(phi,x);
     }
 
 
-    FILE *f = fopen("pot_test.dat","w");
 
-    fwrite(res,sizeof(double complex),NM*NR,f);
-    fclose(f);
+
+    fft_init(in,out,nphi);
+            
+    do_fft(in,out);
+    printf("%lg\n",creal(out[1]));
+
+
+    free_fft(in,out);
 
 
 
